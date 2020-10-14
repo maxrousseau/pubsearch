3a3da4b1ef04e4331782ff867c1b498bb366ffca
Maxime Rousseau <maximerousseau08@gmail.com>
2020-10-13 20:22:14 -0400

    minor improvements, still runtime segfault...

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 
 /*
 Replace the stupid std stream stuff with fmt because the other one is insane
-also use conanfile.txt and integrate with cmake to make dependency management
+nnalso use conanfile.txt and integrate with cmake to make dependency management
 easier
 
 Create citation struct write all citations to file
@@ -23,12 +23,13 @@ Create citation struct write all citations to file
        page = "",
  }
 
-plan for this little project
+Plan for this little project
 1. [x] build your query (some decent defaults for retmax and other categories)
 2. [ ] vector of paper IDs
 3. [ ] get the summary of the papers
 4. [ ] get the references in bibtex fmt
-5. [ ] return to std_out, export (), ...
+5. [ ] fmt output (info + abstract), export to bibtex, ...
+6. [ ] implement the cli arguments
 
 */
 
@@ -45,28 +46,28 @@ struct Query
   std::string abstract = "rettype=abstract";
 };
 
+enum queryType { search, fetch, info };
+
+
 // pass type argument and the query struct
 // > check if type is search or summary or author info
 //
-std::string buildQuery(std::string type)
+std::string buildQuery(queryType qt)
 {
+  
   Query q;
   std::stringstream ss;
-  //TODO convert this to a switch statement
-  if (type == "search") {
-	ss << q.esearch << q.db << q.term << q.retmax << q.json;
-  }
-  //TODO make the IDlisit!!!
-  else if (type == "summary") {
-	// ss << q.efetch << q.db << q.id_list << q.text << q.abstract;
-  }
-  //TODO add the info statement 
-  else {
-	std::cout << "should not run\n";
-  }
+
+  switch(qt)
+  {
+  case search : ss << q.esearch << q.db << q.term << q.retmax << q.json; break;
+  case fetch : ss << q.efetch << q.db << "q.id_list" << q.text << q.abstract; break;
+	// TODO -> figure out how to make the id_list vector
+  case info : ss << "nothing yet ... \n"; break;
+  };
+
 
   std::string query = ss.str();
-
   return query;
 }
 
@@ -77,21 +78,22 @@ void getSummary(id_list)
 
 void exportBibtex()
 {
-}
+}p
 */
 
 int main()
 {
 
-    // little test
-  std::string test = buildQuery("search");
-  cpr::Response r = cpr::Get(cpr::Url{test});
+  // dummy example
+  std::string test = buildQuery(search);
+  cpr::Response r = cpr::Get(cpr::Url{test}); //for some reason this is
+											  //segfaulting at runtime... maybe
+											  //this is a cpr:: issue?
 
   auto json = nlohmann::json::parse(r.text);
-  std::cout << json.dump(4);
-  //auto id_list = json["esearchresult"];
-  //std::cout << id_list;
-  //std::cout << r.text;
-
+  auto id_list = json["esearchresult"]["idlist"];
+  std::cout << id_list << "\n"; //ok im getting ID list, I need to turn this
+								//into a vector of strings...
+  
   return 0;
 }
